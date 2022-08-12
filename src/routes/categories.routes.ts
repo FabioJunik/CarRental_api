065a60c356765d69f6f33200329ad8e45e59da84
FabioJunik <fabiojunik@gmail.com>
2022-08-12 17:32:13 +0100

    Insrindo ID com uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,20 @@
-import {response, Router} from "express";
+import {Router} from "express";
+import {v4 as uuidv4} from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response)=>{
+categoriesRoutes.post("/", (request, response)=>{
     const {name, description} = request.body;
     
-    categories.push({
+    const category = {
+        id: uuidv4(),
         name,
         description
-    });
+    };
+
+    categories.push(category);
 
     return response.status(201).send();    
 });
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,6 @@ import {categoriesRoutes} from './routes/categories.routes';
 const app = express();
 
 app.use(express.json());
-app.use(categoriesRoutes);
+app.use("/categories",categoriesRoutes);
 
 app.listen(5000);
