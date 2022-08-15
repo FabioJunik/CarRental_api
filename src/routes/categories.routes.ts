e5dc62f0644777980822ca4c29cd4d63717eff84
FabioJunik <fabiojunik@gmail.com>
2022-08-15 08:27:25 +0100

    Não permitindo categorias com o mesmo nome

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response)=>{
     const {name, description} = request.body;
 
+    const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+    if(categoryAlreadyExists){
+        return response.status(400).json({error: "Category Already exists!"})
+    }
+
     categoriesRepository.create({name, description});    
 
     return response.status(201).send();    
