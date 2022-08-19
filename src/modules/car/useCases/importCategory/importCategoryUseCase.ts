36b6e0496bbe1e9a2690000380dc2a13c3be9e27
FabioJunik <fabiojunik@gmail.com>
2022-08-19 22:26:54 +0100

    Removendo os arquivos de upload

--- src/modules/car/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/car/useCases/importCategory/importCategoryUseCase.ts
@@ -23,6 +23,7 @@ class ImportCategoryUseCase{
                 categories.push({name, description});
             }).
             on('end',  ()=>{
+                fs.promises.unlink  (file.path);
                 resolse(categories)
             })
             .on('error',(error)=>{
