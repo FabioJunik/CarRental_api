3d865f5c4fa8a5977a79911644ca585d3ca5fc83
FabioJunik <fabiojunik@gmail.com>
2022-08-19 18:09:54 +0100

    Usando o Stream para ler arquivos

--- src/modules/car/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/car/useCases/importCategory/importCategoryUseCase.ts
@@ -1,8 +1,17 @@
-
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
 
 class ImportCategoryUseCase{
-    execute(file:any){
-        console.log(file);
+    execute(file: Express.Multer.File){
+
+        const stream = fs.createReadStream(file.path);
+        const parseFile = csvParse();
+        
+        stream.pipe(parseFile);
+
+        parseFile.on('data', async (line)=>{
+            console.log(line);
+        })
     }
 }
 
